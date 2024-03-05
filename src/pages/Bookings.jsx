98957838c59302f2f8fbbeacb9818f1fb99d2d83
luis <luisnavarroFS@gmail.com>
2024-03-05 22:13:56 +0100

    feat: create table

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,6 +1,57 @@
+import data from '../../data.json'
+
 function Bookings() {
+    const columns = [
+        {
+            label: "Guest",
+            display: row => `${row.first_name} ${row.last_name}`
+        },
+        {
+            label: 'Order Date',
+            property: 'order_date'
+        },
+        {
+            label: 'Check In',
+            property: 'check_in'
+        },
+        {
+            label: 'Check Out',
+            property: 'check_out'
+        },
+        {
+            label: 'Room Type',
+            property: 'room_type'
+        },
+        {
+            label: 'Status',
+            property: 'status'
+        }
+    ]
+
     return (
-        <div>Bookings</div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => <th key={index}>{column.label}</th>)}
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {data.map((row, index) => {
+                        return <tr key={index}>
+                            {columns.map((column, i) => {
+                                return (
+                                    <td key={i}>
+                                        {row[column.property] ? row[column.property] : column.display(row)}
+                                    </td>
+                                )
+                            })}
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
