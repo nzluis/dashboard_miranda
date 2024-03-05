f69fe794aea9baefbf5ad91791178ce356399bf1
luis <luisnavarroFS@gmail.com>
2024-03-05 18:28:13 +0100

    feat:  sidebar styledComp

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,20 +1,23 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const SideBar = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 25px;
+    margin-left: 25px;
+    grid-area: panel;
+`
 
 export default function Panel() {
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            gap: '25px',
-            marginLeft: '25px',
-            gridArea: 'panel'
-        }}>
+        <SideBar>
             <NavLink to='/'>Dashboard</NavLink>
             <NavLink to='/rooms'>Rooms</NavLink>
             <NavLink to='/bookings'>Bookings</NavLink>
             <NavLink to='/users'>Users</NavLink>
             <NavLink to='/contact'>Contact</NavLink>
-        </div>
+        </SideBar>
     )
 }
