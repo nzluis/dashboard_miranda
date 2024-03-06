307c9795da89dbf9ceff43cb417f9ab25b7545e4
luis <luisnavarroFS@gmail.com>
2024-03-06 12:28:12 +0100

    rooms table

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,5 +1,45 @@
+import rooms from '../../rooms.json'
+import DataTable from "../components/DataTable"
+
 export default function Rooms() {
+    const columns = [
+        {
+            label: "Room Name",
+            display: row =>
+                <div style={{ display: 'flex' }}>
+                    <img src={row.photo} />
+                    <div>
+                        <p>{row.id}</p>
+                        <p>{row.room_number}</p>
+                    </div>
+                </div>
+        },
+        {
+            label: 'Room Type',
+            property: 'room_type'
+        },
+        {
+            label: 'Amenities',
+            display: row => `${row.amenities.slice(0, 10)} ...`
+        },
+        {
+            label: 'Price',
+            property: 'price'
+        },
+        {
+            label: 'Offer',
+            property: 'offer_price'
+        },
+        {
+            label: 'Status',
+            property: 'status'
+        },
+    ]
     return (
-        <div>Rooms</div>
+        <div style={{ overflow: 'scroll' }}>
+            <h1>Rooms</h1>
+            <DataTable data={rooms} columns={columns} />
+        </div>
+
     )
 }
