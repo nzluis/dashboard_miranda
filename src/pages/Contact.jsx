191dd943d995029ae2c4e68b480bea0b01d1d9c9
luis <luisnavarroFS@gmail.com>
2024-03-06 13:35:00 +0100

    feat: contacts table

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,45 @@
+import contacts from '../assets/data/contacts.json'
+import { DashBoard } from '../assets/styled Components/DashBoard'
+import DataTable from "../components/DataTable"
+
 function Contact() {
+    const columns = [
+        {
+            label: 'Comment ID',
+            property: 'id'
+        },
+        {
+            label: 'Date',
+            display: row => new Date(Number(row.date)).toLocaleDateString('es-Es')
+        },
+        {
+            label: 'Customer',
+            display: row =>
+                <div>
+                    <p>{row.full_name}</p>
+                    <p>{row.phone}</p>
+                    <p>{row.email}</p>
+                </div>
+        },
+        {
+            label: 'Comment',
+            display: row =>
+                <div>
+                    <h5>{row.subject}</h5>
+                    <p>{row.message.slice(0, 55)} ...</p>
+                </div>
+        },
+        {
+            label: 'Status',
+            property: 'status'
+        }
+    ]
+
     return (
-        <div>Contact</div>
+        <DashBoard>
+            <h1>Contact</h1>
+            <DataTable data={contacts} columns={columns} />
+        </DashBoard>
     )
 }
 
