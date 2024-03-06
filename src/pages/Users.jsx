01a376cec9ec199b13245e5c1a09607fdc1679a4
luis <luisnavarroFS@gmail.com>
2024-03-06 12:57:17 +0100

    feat: users table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,6 +1,40 @@
+import users from '../../users.json'
+import DataTable from "../components/DataTable"
+
 function Users() {
+    const columns = [
+        {
+            label: "Name",
+            display: row =>
+                <div style={{ display: 'flex' }}>
+                    <img src={row.photo} />
+                    <div>
+                        <p>{row.full_name}</p>
+                        <p>{row.id}</p>
+                        <p>joined on {row.start_date}</p>
+                        <p>{row.email}</p>
+                    </div>
+                </div>
+        },
+        {
+            label: 'Description',
+            display: row => `${row.description.slice(0, 10)} ...`
+        },
+        {
+            label: 'Contact',
+            property: 'contact'
+        },
+        {
+            label: 'Status',
+            property: 'status'
+        },
+    ]
+
     return (
-        <div>Users</div>
+        <div style={{ overflow: 'scroll' }}>
+            <h1>Users</h1>
+            <DataTable data={users} columns={columns} />
+        </div>
     )
 }
 
