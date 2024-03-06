1369352264256fd7f0bbf17078897bfd56fdbf5e
luis <luisnavarroFS@gmail.com>
2024-03-06 23:32:43 +0100

    refactor: login

---
+++ src/style/Form.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%,-50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 30px auto;
+    width: 25%;
+
+  
+    
+    label {
+        display: flex;
+        flex-direction: column;
+        color: var(--padding-second);
+          font-size: 17px;
+    font-weight: 300;
+    line-height: 27px;
+    letter-spacing: 5px;
+        
+    }
+
+    input {
+        border: none;
+        padding: 10px 15px;
+        border: 1px solid var(--padding-second);
+        border-radius: 12px;
+        margin: 5px 0;
+    }
+    input:focus {
+        outline:none;
+        box-shadow: 0 0 1px 1px var(--padding-second);
+    }
+
+    button {
+        margin-top: 20px;
+        max-width: fit-content ;
+    }
+
+    p {
+        margin-top: 5px;
+        text-align: center;
+        font-size: 12px; 
+        color: var(--padding-first);
+        max-width: 60%
+    }
+`
