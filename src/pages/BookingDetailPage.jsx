834b2d074bbe78ffeea9ef22b6619f8b51e46796
luis <luisnavarroFS@gmail.com>
2024-03-08 21:34:09 +0100

    WIP: booking detail

--- src/pages/BookingDetailPage.jsx
+++ src/pages/BookingDetailPage.jsx
@@ -1,11 +1,46 @@
 import { useParams } from "react-router-dom"
 import { DashBoard } from "../style/DashBoardStyled"
+import { CheckDatesBox, LeftSide, RightSide } from "../style/BookingDetailStyled"
+import data from '../assets/data/bookings.json'
+
+const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+};
 
 export default function BookingDetail() {
     const { id } = useParams()
+    const userData = data[id - 1]
     return (
-        <DashBoard>
-            <h1>Booking number {id}</h1>
+        <DashBoard flex>
+            <LeftSide>
+                <h1>{userData.first_name} {userData.last_name}</h1>
+                <p>{userData.id}</p>
+                <CheckDatesBox>
+                    <CheckDatesBox insidebox>
+                        <h5>Check In</h5>
+                        <p>{new Date(Number(userData.check_in)).toLocaleDateString('en-EN', options)}</p>
+                    </CheckDatesBox>
+                    <CheckDatesBox insidebox>
+                        <h5>Check Out</h5>
+                        <p>{new Date(Number(userData.check_out)).toLocaleDateString('en-EN', options)}</p>
+                    </CheckDatesBox>
+                </CheckDatesBox>
+                <p>{userData.room_type}</p>
+                <p>{userData.price}</p>
+                <p>descripcion</p>
+                <p>325$</p>
+                <p>Servicios</p>
+            </LeftSide>
+
+            <RightSide>
+                <p>carusel de fotos</p>
+                <p>tipo de hbaitacion</p>
+                <p>descripcion habitacion</p>
+                <p>estado</p>
+            </RightSide>
         </DashBoard>
     )
 }
---
+++ src/style/BookingDetailStyled.js
@@ -0,0 +1,35 @@
+import styled, { css } from "styled-components";
+
+const CommonSides = styled.div`
+width: 50%;
+height: 100%;
+padding: 30px;
+`
+
+export const LeftSide = styled(CommonSides)`
+    background-color: white;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    p {
+        &:nth-of-type(1) {
+            font-size: 16px;
+            font-weight: 600;
+            color: var(--panel-inactive)
+        }
+    }
+`
+
+export const CheckDatesBox = styled.div`
+    display: flex;
+    gap: 25px;
+    ${props => props.insidebox && css`
+        flex-direction: column;
+        gap: 5px
+    `}
+
+`
+export const RightSide = styled(CommonSides)`
+    background-color: var(--padding-first);
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+`
