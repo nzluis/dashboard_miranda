7f2b8423d77efc559015c9b8f1c88d419df1536a
luis <luisnavarroFS@gmail.com>
2024-03-13 14:54:40 +0100

    fix: formbooking

--- src/pages/FormBookingPage.jsx
+++ src/pages/FormBookingPage.jsx
@@ -9,92 +9,101 @@ import { bookingsByIdData } from "../features/bookings/bookingsSlice";
 import { LinearProgress } from "@mui/material";
 
 export default function FormBookingPage() {
-    const bookingData = useSelector(bookingsByIdData)
-    const [firstName, setFirstName] = useState(bookingData.first_name || undefined)
-    const [lastName, setLastName] = useState(bookingData.last_name || undefined)
-    const [checkIn, setCheckIn] = useState(bookingData.check_in || Date.now())
-    const [checkOut, setCheckOut] = useState(bookingData.check_out || Date.now())
-    const [request, setRequest] = useState(bookingData.request || undefined)
-    const [roomType, setRoomType] = useState(bookingData.room_type || 'Single Bed')
-    const [status, setStatus] = useState(bookingData.status || 'In Progress')
-    const [roomNumber, setRoomNumber] = useState(bookingData.room_number || undefined)
     const dispatch = useDispatch()
     const { id } = useParams()
-    console.log(bookingData)
     const [fetched, setFetched] = useState(false)
-    if (id) {
-        const initialFetch = async () => {
-            await dispatch(fetchBookingById(Number(id)))
-            setFetched(true)
-        }
-
-        useEffect(() => {
-            initialFetch()
-        }, [])
-
-        if (!fetched) return <LinearProgress />
-    }
-    const newBooking = {
+    const booking = {
         id: Math.round(Math.random() * 1000),
-        first_name: firstName,
-        last_name: lastName,
+        first_name: '',
+        last_name: '',
         order_date: new Date(Date.now()).getTime(),
-        check_in: new Date(checkIn).getTime(),
-        check_out: new Date(checkOut).getTime(),
-        request,
-        room_type: roomType,
-        room_number: roomNumber,
-        status,
+        check_in: '',
+        check_out: '',
+        request: '',
+        room_type: 'Single Bed',
+        room_number: '',
+        status: 'In Progress'
+    }
+
+
+    const [formData, setFormData] = useState({
+        id: Math.round(Math.random() * 1000),
+        first_name: '',
+        last_name: '',
+        order_date: new Date(Date.now()).getTime(),
+        check_in: '',
+        check_out: '',
+        request: '',
+        room_type: 'Single Bed',
+        room_number: '',
+        status: 'In Progress'
+    })
+
+    const initialFetch = async () => {
+        await dispatch(fetchBookingById(Number(id))).unwrap()
+        setFetched(true)
+    }
+
+    useEffect(() => {
+        if (id) initialFetch()
+    }, [])
+
+    useEffect(() => {
+        if (id && booking) setFormData(booking)
+    }, [booking])
+
+    if (!fetched && id) return <LinearProgress />
+
+    function handleChange(e) {
+        const { name, value, type } = e.target
+        if (type === 'date') setFormData(prevFormData => ({ ...prevFormData, [name]: value }))
+        else setFormData(prevFormData => ({ ...prevFormData, [name]: value }))
     }
 
     function handleSubmit(e) {
         e.preventDefault()
-        if (id) {
-            dispatch(updateBooking(newBooking))
-        } else {
-            console.log(newBooking)
-            dispatch(createBooking(newBooking))
-        }
+        dispatch(createBooking(formData))
     }
 
+    console.log(formData)
     return (
         <DashBoard>
             <Form>
                 <label htmlFor="first_name">First Name:
                     <input
 
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
+                        value={formData.first_name}
+                        onChange={handleChange}
                         name="first_name"
                     />
                 </label>
                 <label htmlFor="last_name">Last Name:
                     <input
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
+                        value={formData.last_name}
+                        onChange={handleChange}
                         name="last_name"
                     />
                 </label>
                 <label htmlFor="check_in">Check In:
                     <input
-                        value={checkIn}
-                        onChange={(e) => setCheckIn(e.target.value)}
+                        value={formData.check_in}
+                        onChange={handleChange}
                         name="check_in"
                         type="date"
                     />
                 </label>
                 <label htmlFor="check_out">Check Out:
                     <input
-                        value={checkOut}
-                        onChange={(e) => setCheckOut(e.target.value)}
+                        value={formData.check_out}
+                        onChange={handleChange}
                         name="check_out"
                         type="date"
                     />
                 </label>
                 <label htmlFor="request">Message:
                     <textarea
-                        value={request}
-                        onChange={(e) => setRequest(e.target.value)}
+                        value={formData.request}
+                        onChange={handleChange}
                         name="request"
                         style={{ height: '125px' }}
                     />
@@ -102,11 +111,11 @@ export default function FormBookingPage() {
                 <FormRow>
                     <label htmlFor="room_type">Room Type:
                         <select
-                            value={roomType}
-                            onChange={(e) => setRoomType(e.target.value)}
+                            value={formData.room_type}
+                            onChange={handleChange}
                             name="check_out"
                         >
-                            <option value="Dingle Bed">Single Bed</option>
+                            <option value="Single Bed">Single Bed</option>
                             <option value="Double Bed">Double Bed</option>
                             <option value="Double Superior">Double Superior</option>
                             <option value="Suite">Suite</option>
@@ -114,8 +123,8 @@ export default function FormBookingPage() {
                     </label>
                     <label htmlFor="room_number">Room Number:
                         <input
-                            value={roomNumber}
-                            onChange={(e) => setRoomNumber(e.target.value)}
+                            value={formData.room_number}
+                            onChange={handleChange}
                             name="room_number"
                             type="number"
                         />
@@ -123,8 +132,8 @@ export default function FormBookingPage() {
                 </FormRow>
                 <label htmlFor="status">Status:
                     <select
-                        value={status}
-                        onChange={(e) => setStatus(e.target.value)}
+                        value={formData.status}
+                        onChange={handleChange}
                         name="status"
                     >
                         <option value="In Progress">In Progress</option>
