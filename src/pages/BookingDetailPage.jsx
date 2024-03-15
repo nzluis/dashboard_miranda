3e1f94390a9fe637907c124973a91a29c68b06a1
luis <luisnavarroFS@gmail.com>
2024-03-15 21:12:58 +0100

    WIP: booking detail

--- src/pages/BookingDetailPage.jsx
+++ src/pages/BookingDetailPage.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom"
 import { DashBoard } from "../style/DashBoardStyled"
 import { CheckDatesBox, LeftSide, RightSide } from "../style/BookingDetailStyled"
 import { bookingByIdData } from "../features/bookings/bookingsSlice";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { fetchBookingById } from "../features/bookings/bookingsThunk";
 import { LinearProgress } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchRooms } from "../features/rooms/roomsThunk";
+import { roomsData } from "../features/rooms/roomsSlice";
 
 const options = {
     weekday: "long",
@@ -17,20 +19,30 @@ const options = {
 export default function BookingDetail() {
     const { id } = useParams()
     const bookingData = useSelector(bookingByIdData)
+    const rooms = useSelector(roomsData)
+    const daysBetween = useMemo(() => {
+        if (bookingData)
+            return Math.ceil(Math.abs(bookingData.check_out - bookingData.check_in) / 1000 / 60 / 60 / 24)
+    }, [bookingData])
+    const room = useMemo(() => {
+        if (rooms && bookingData) {
+
+            return rooms.find(room => room.room_type == bookingData.room_type)
+        }
+    }, [rooms])
+
     const dispatch = useDispatch()
     const [fetched, setFetched] = useState(false)
-
-
     const initialFetch = async () => {
-        await dispatch(fetchBookingById(id))
+        await dispatch(fetchBookingById(id)).unwrap()
+        await dispatch(fetchRooms()).unwrap()
         setFetched(true)
     }
 
     useEffect(() => {
         initialFetch()
     }, [])
-
-    if (!fetched) return <LinearProgress />
+    if (!fetched || !room) return <LinearProgress />
 
     return (
         <DashBoard $flex>
@@ -47,18 +59,18 @@ export default function BookingDetail() {
                         <p>{new Date(Number(bookingData.check_out)).toLocaleDateString('en-EN', options)}</p>
                     </CheckDatesBox>
                 </CheckDatesBox>
-                <p>{bookingData.room_type}</p>
+                <p>{bookingData.room_number}</p>
                 <p>{bookingData.price}</p>
-                <p>{bookingData.description}</p>
-                <p>{bookingData.price}$</p>
-                <p>Servicios</p>
+                <p>{room.request}</p>
+                <p>{room.price * daysBetween} <span>$ </span></p>
+                <p>{room.amenities.join(', ')}</p>
             </LeftSide>
 
             <RightSide>
-                <p>carusel de fotos</p>
-                <p>tipo de hbaitacion</p>
-                <p>descripcion habitacion</p>
-                <p>estado</p>
+                <img src="../../public/room1.jpg" />
+                <p>{room.room_type}</p>
+                <p>{room.description}</p>
+                <p>{room.status}</p>
             </RightSide>
         </DashBoard>
     )
