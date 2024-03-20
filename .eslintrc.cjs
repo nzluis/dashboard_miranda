ea1329e79bbea0689466dfb72a2b41b4e9196662
luis <luisnavarroFS@gmail.com>
2024-03-20 11:43:05 +0100

    feat: set up jest & rtl

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "react-app", "react-app/jest"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-     "react/prop-types": "off"
+    "react/prop-types": "off",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
