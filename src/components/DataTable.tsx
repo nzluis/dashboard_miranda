65ce0b3bc092ff5cf12c781b8e0dff9f0d5c2182
luis <luisnavarroFS@gmail.com>
2024-03-27 19:07:01 +0100

    feat: ts datatable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,8 +1,20 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import { StyledTable, StyledTableBody, StyledTableHead, TdActions } from "../style/TableStyled"
 import { FiEdit, FiXCircle } from "react-icons/fi";
+import { RoomData } from "../interfaces/Rooms";
+import { BookingData } from "../interfaces/Bookings";
+import { ContactData } from "../interfaces/Contacts";
+import { UserData } from "../interfaces/Users";
 
-export default function DataTable({ data, columns, actions, position, noPointer }) {
+interface DataTableProps {
+    data: BookingData[] | RoomData[] | ContactData[] | UserData[]
+    columns: { label: string, property?: string, display?: Function }[]
+    actions: { name: string, handler: Function }[]
+    position?: string
+    noPointer?: boolean
+}
+
+export default function DataTable({ data, columns, actions, position, noPointer }: DataTableProps) {
     const { pathname } = useLocation()
     const navigate = useNavigate()
     return (
@@ -14,14 +26,14 @@ export default function DataTable({ data, columns, actions, position, noPointer 
                 </tr>
             </StyledTableHead>
 
-            <StyledTableBody $position={position} $noPointer={noPointer}>
-                {data.map((row, index) => {
+            <StyledTableBody $position={position as string} $noPointer={noPointer as boolean}>
+                {data.map((row: BookingData | RoomData | UserData | ContactData, index: number) => {
                     return (
                         <tr key={index} onClick={() => pathname === '/bookings' && navigate(`${pathname}/${row.id}`)}>
                             {columns.map((column, i) => {
                                 return (
                                     <td key={i}  >
-                                        {row[column.property] ? row[column.property] : column.display(row)}
+                                        {row[column.property as keyof BookingData & keyof RoomData & keyof ContactData & keyof UserData] ? row[column.property as keyof BookingData & keyof RoomData & keyof ContactData & keyof UserData] : column.display!(row)}
                                     </td>
                                 )
                             })}
