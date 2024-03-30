0bbbfc8c0c4f0996880aef44fd5b064392069824
luis <luisnavarroFS@gmail.com>
2024-03-30 13:33:40 +0100

    feat: ts users

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,7 +1,7 @@
 import { DashBoard } from '../style/DashBoardStyled'
 import DataTable from "../components/DataTable"
 import { useNavigate } from 'react-router-dom'
-import { useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, SyntheticEvent, useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ButtonActive } from '../style/ButtonStyled';
 import { Tab, TabsContainer } from '../style/TopMenuStyled';
@@ -13,12 +13,14 @@ import { usersData } from '../features/users/usersSlice';
 import { LinearProgress } from '@mui/material';
 import { deleteUserById, fetchUsers } from '../features/users/usersThunk';
 import Pagination from '../components/Pagination';
+import { UserData } from '../interfaces/Users';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
 
 function Users() {
     const columns = [
         {
             label: "Name",
-            display: row =>
+            display: (row: UserData) =>
                 <div style={{ display: 'flex', gap: '15px' }}>
                     <img src={row.photo} />
                     <div>
@@ -31,7 +33,7 @@ function Users() {
         },
         {
             label: 'Description',
-            display: row =>
+            display: (row: UserData) =>
                 <div className='moreLines'>
                     <h5>{row.position}</h5>
                     <p>{row.description.slice(0, 100)}</p>
@@ -39,14 +41,14 @@ function Users() {
         },
         {
             label: 'Contact',
-            display: row =>
+            display: (row: UserData) =>
                 <div>
                     {row.phone}
                 </div>
         },
         {
             label: 'Status',
-            display: row =>
+            display: (row: UserData) =>
                 <>
                     <div
                         className={
@@ -60,12 +62,12 @@ function Users() {
         },
     ]
 
-    const deleteUser = (e, user) => {
+    const deleteUser = (e: SyntheticEvent, user: UserData) => {
         e.stopPropagation()
         dispatch(deleteUserById(user.id))
     }
 
-    const editUser = (e, user) => {
+    const editUser = (e: SyntheticEvent, user: UserData) => {
         e.stopPropagation()
         navigate(`/users/edit/${user.id}`)
     }
@@ -76,13 +78,13 @@ function Users() {
     ]
 
     const navigate = useNavigate()
-    const dispatch = useDispatch()
+    const dispatch = useAppDispatch()
     const tabs = ['All Users', 'Active', 'Inactive']
     const orderTags = ['start_date', 'start_date_DESC', 'full_name', 'full_name_DESC']
     const [selectedTab, setSelectedTab] = useState('All Users')
     const [orderBy, setOrderBy] = useState('start_date')
     const [fetched, setFetched] = useState(false)
-    const allUsers = useSelector(usersData)
+    const allUsers = useAppSelector(usersData)
     const users = useMemo(() => {
         const users = allUsers.filter(user => selectedTab === 'All Users' ? true : user.status === selectedTab)
         return users.sort((a, b) => {
@@ -109,7 +111,7 @@ function Users() {
     const totalPages = Math.ceil(users.length / 10)
 
     const initialFetch = async () => {
-        await dispatch(fetchUsers()).unwrap()
+        await dispatch(fetchUsers())
         setFetched(true)
     }
 
@@ -118,7 +120,7 @@ function Users() {
     }, [])
 
     const [stateIndex, setStateIndex] = useState(0)
-    function handleTab(tab, index) {
+    function handleTab(tab: string, index: number) {
         setPage(1)
         tab === selectedTab ? setSelectedTab('All Users') : setSelectedTab(tab)
         stateIndex === index ? setStateIndex(0) : setStateIndex(index)
@@ -134,7 +136,7 @@ function Users() {
                 </TabsContainer>
                 <ButtonsContainer>
                     <ButtonActive onClick={() => navigate('/users/new-user')}>+ New User</ButtonActive>
-                    <SelectOrder onChange={(e) => setOrderBy(e.target.value)}>
+                    <SelectOrder onChange={(e: ChangeEvent<HTMLSelectElement>) => setOrderBy(e.target.value)}>
                         {orderTags.map((tag, index) => {
                             return <option
                                 key={index}
