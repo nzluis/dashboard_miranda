a16545fe914354860b210dcd682f3e2f90aed9bd
luis <luisnavarroFS@gmail.com>
2024-03-30 13:29:27 +0100

    feat: ts navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,17 @@ import { HiOutlineBars3BottomLeft } from "react-icons/hi2";
 import { NavBar, NavIcons } from "../style/NavbarStyled";
 import { useAuth } from "../context/AuthContext";
 import { Link } from 'react-router-dom'
+import { Dispatch, SetStateAction } from "react";
 
-export default function Navbar({ visiblePanel, setVisiblePanel }) {
+interface NavbarProps {
+    visiblePanel: boolean
+    setVisiblePanel: Dispatch<SetStateAction<boolean>>
+}
+
+export default function Navbar({ visiblePanel, setVisiblePanel }: NavbarProps) {
     const { state, dispatch } = useAuth()
     const { pathname } = useLocation()
-    const regexMatch = pathname.match(/\/[^/]+/g)
+    const regexMatch: RegExpMatchArray | null = pathname.match(/\/[^/]+/g)
     const titleName = pathname[1] ? regexMatch[0][1].toUpperCase() + regexMatch[0].slice(2) : 'Dashboard'
     const subTitleName = regexMatch !== null && regexMatch[1] ? regexMatch[1].replace('-', ' ').slice(1) : false
 
@@ -22,20 +28,19 @@ export default function Navbar({ visiblePanel, setVisiblePanel }) {
         <NavBar>
             <div>
                 {visiblePanel ? <HiOutlineBars3BottomLeft size={26} onClick={togglePanel} /> : <FaArrowRight size={26} onClick={togglePanel} />}
-                <Link to={titleName !== 'Dashboard' && `/${titleName.toLowerCase()}`}>
+                <Link to={titleName !== 'Dashboard' ? `/${titleName.toLowerCase()}` : ''}>
                     <h1>{titleName === 'Login' ? '' : titleName}</h1>
                 </Link>
-                {subTitleName && <h2>{subTitleName.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')}</h2>}
+                {subTitleName && <h2>{subTitleName.split(' ').map((word: string) => word[0].toUpperCase() + word.slice(1)).join(' ')}</h2>}
             </div>
             <NavIcons>
                 <NavLink to="/contact" >
                     <FaRegEnvelope size={26} />
                 </NavLink>
-                <NavLink>
-
+                <NavLink to=''>
                     <FaRegBell size={26} />
                 </NavLink>
-                <NavLink>
+                <NavLink to=''>
                     {state.isAuthenticated && <PiSignOutBold size={26} onClick={() => dispatch({ type: 'LOGOUT' })} />}
                 </NavLink>
 
