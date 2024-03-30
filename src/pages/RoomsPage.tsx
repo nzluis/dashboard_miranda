881ef437233681423b41d3deb5c882a4eb8f5b07
luis <luisnavarroFS@gmail.com>
2024-03-30 13:33:23 +0100

    feat: ts rooms

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -2,14 +2,16 @@ import { DashBoard } from '../style/DashBoardStyled'
 import DataTable from "../components/DataTable"
 import { useNavigate } from 'react-router-dom'
 import { ButtonActive } from '../style/ButtonStyled'
-import { useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, SyntheticEvent, useEffect, useMemo, useState } from 'react'
 import { roomsData } from '../features/rooms/roomsSlice'
 import usePaginate from '../../hooks/usePaginate'
-import { useDispatch, useSelector } from 'react-redux'
 import { deleteRoomById, fetchRooms } from '../features/rooms/roomsThunk'
 import { ButtonsContainer, SelectOrder, Tab, TabsContainer, TopMenu } from '../style/TopMenuStyled'
 import { LinearProgress } from '@mui/material'
 import Pagination from '../components/Pagination'
+import { RoomData } from '../interfaces/Rooms';
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+
 
 export default function Rooms() {
     const navigate = useNavigate()
@@ -17,7 +19,7 @@ export default function Rooms() {
     const columns = [
         {
             label: "Room Name",
-            display: row =>
+            display: (row: RoomData) =>
                 <div style={{ display: 'flex', gap: '5px' }}>
                     <img src={row.photo} />
                     <div>
@@ -32,21 +34,21 @@ export default function Rooms() {
         },
         {
             label: 'Amenities',
-            display: row =>
+            display: (row: RoomData) =>
                 <div className='twoLines'>
                     <p>{row.amenities.toString().slice(0, 21) + '...'}</p>
                 </div>
         },
         {
             label: 'Price',
-            display: row =>
+            display: (row: RoomData) =>
                 <div>
                     <span className='highlight'>{Number(row.price)}€</span><span className='panelColor'>/Night</span>
                 </div>
         },
         {
             label: 'Offer',
-            display: row =>
+            display: (row: RoomData) =>
                 <div>
                     {row.offer ?
                         <><span className='highlight'>{Math.round(Number(row.price) * (1 - Number(row.discount) / 100))}€</span><span className='panelColor'>/Night</span></>
@@ -57,7 +59,7 @@ export default function Rooms() {
         },
         {
             label: 'Status',
-            display: row =>
+            display: (row: RoomData) =>
                 <>
                     <div
                         className={
@@ -71,12 +73,12 @@ export default function Rooms() {
         },
     ]
 
-    const deleteRoom = (e, room) => {
+    const deleteRoom = (e: SyntheticEvent, room: RoomData) => {
         e.stopPropagation()
         dispatch(deleteRoomById(room.id))
     }
 
-    const editRoom = (e, room) => {
+    const editRoom = (e: SyntheticEvent, room: RoomData) => {
         e.stopPropagation()
         navigate(`/rooms/edit/${room.id}`)
     }
@@ -92,8 +94,8 @@ export default function Rooms() {
     const [selectedTab, setSelectedTab] = useState('All Rooms')
     const [orderBy, setOrderBy] = useState('low_to_high')
     const [fetched, setFetched] = useState(false)
-    const dispatch = useDispatch()
-    const allRooms = useSelector(roomsData)
+    const dispatch = useAppDispatch()
+    const allRooms = useAppSelector(roomsData)
     const rooms = useMemo(() => {
         const rooms = selectedTab === 'All Rooms' ?
             allRooms :
@@ -120,7 +122,7 @@ export default function Rooms() {
     const totalPages = Math.ceil(rooms.length / 10)
 
     const initialFetch = async () => {
-        await dispatch(fetchRooms()).unwrap()
+        await dispatch(fetchRooms())
         setFetched(true)
     }
 
@@ -129,7 +131,7 @@ export default function Rooms() {
     }, [])
 
     const [stateIndex, setStateIndex] = useState(0)
-    function handleTab(tab, index) {
+    function handleTab(tab: string, index: number) {
         setPage(1)
         tab === selectedTab ? setSelectedTab('All Rooms') : setSelectedTab(tab)
         stateIndex === index ? setStateIndex(0) : setStateIndex(index)
@@ -145,7 +147,7 @@ export default function Rooms() {
                 </TabsContainer>
                 <ButtonsContainer>
                     <ButtonActive onClick={() => navigate('/rooms/new-room')}>+ New Room</ButtonActive>
-                    <SelectOrder onChange={(e) => setOrderBy(e.target.value)}>
+                    <SelectOrder onChange={(e: ChangeEvent<HTMLSelectElement>) => setOrderBy(e.target.value)}>
                         {orderTags.map((tag, index) => {
                             return <option
                                 key={index + 1}
