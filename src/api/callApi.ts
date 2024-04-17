03936ea80ba1cf9e50ed7afc2cbb92e6abf9fbca
luis <luisnavarroFS@gmail.com>
2024-04-17 19:17:46 +0200

    feat: auth token control on calling api

--- src/api/callApi.ts
+++ src/api/callApi.ts
@@ -1,27 +1,30 @@
+import { AuthState } from "../interfaces/Auth"
 import { BookingData } from "../interfaces/Bookings"
 import { ContactData } from "../interfaces/Contacts"
 import { RoomData } from "../interfaces/Rooms"
 import { UserData } from "../interfaces/Users"
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
-const token = `Bearer ${import.meta.env.VITE_DEV_TOKEN}`
 type Data = BookingData | RoomData | ContactData | UserData
 
+
 export const callApi = async (path: string, method: string = 'GET', data: Data | string | null = null) => {
+    const auth: AuthState = JSON.parse(localStorage.getItem("USER_AUTH") as string)
     try {
         const options = {
             method,
             headers: {
-                "Authorization": token,
+                "Authorization": auth.user ? `Bearer ${auth.user.token}` : 'noToken',
                 "Content-type": "application/json"
             },
             body: data ? JSON.stringify(data) : null
         }
         const response = await fetch(`${BASE_URL}/${path}`, options)
-        const json = response.json()
-        if (!response.ok) console.error(`Server return a ${response.status} error code`)
+        const json = await response.json()
+        if (!response.ok) console.error(`Server returns ${json.statusCode} error: "${json.message}" `)
         else return json
     } catch (error: any) {
         throw new Error(error)
     }
 }
+
